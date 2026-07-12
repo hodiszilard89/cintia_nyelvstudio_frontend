ac2a7bf2c12d95e6b0a163f113d2028197fa28e7
hodiszilard89 <hodiszilard89@gmail.com>
2026-07-12 11:36:21 +0200

    07.12

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://nyelviskola-env-2.eba-unsp3j22.eu-north-1.elasticbeanstalk.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
